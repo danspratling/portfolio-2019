afd19ba479ed709d08cdbb6bfeaca6b4e7b25fc7
Dan Spratling <daniel.spratling@toumetis.com>
2020-04-13 16:24:29 +0100

    Updates 404 page

--- web/src/pages/404.js
+++ web/src/pages/404.js
@@ -1,14 +1,56 @@
-// import React from "react"
+import React from 'react'
+import { Link } from 'gatsby'
 
-// import Layout from "../components/layout"
-// import SEO from "../components/seo"
+import {
+  Layout,
+  MegaHeading,
+  ProjectCard,
+  // SEO
+  SectionIntro,
+  Social,
+} from '../components'
 
-// const NotFoundPage = () => (
-//   <Layout>
-//     <SEO title="404: Not found" />
-//     <h1>NOT FOUND</h1>
-//     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-//   </Layout>
-// )
+const errorPage = () => {
+  return (
+    <Layout>
+      {/* <SEO title="Home" /> */}
 
-// export default NotFoundPage
+      <section
+        id="intro"
+        className="relative h-screen min-h-1024 bg-black pt-40 pb-40"
+      >
+        <div className="container mx-auto">
+          <div className="grid grid-cols-5 gap-10">
+            <div className="col-span-3">
+              <MegaHeading>
+                404
+                <br />
+                Game Over
+              </MegaHeading>
+              <Social />
+            </div>
+            <div className="col-span-2 z-10">
+              <SectionIntro
+                data={{
+                  heading: 'Mission failed',
+                  title: 'Snake? Snake?! SNAAAAKE!',
+                  body: `<p className="pb-4">
+                        There's no page at <i>${window.location.href}</i>.
+                        <br />
+                        Don't abandon your mission! Go back and try again.
+                      </p>`,
+                  link: {
+                    url: '/',
+                    title: 'Continue',
+                  },
+                }}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  )
+}
+
+export default errorPage
