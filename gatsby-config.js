48542b7f29460259b5f45d79d40cd772dfb7b46c
Dan Spratling <daniel.spratling@toumetis.com>
2020-04-16 17:45:00 +0100

    Adjusts env var names

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,8 +37,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: process.env.CONTENTFUL_SPACEID,
-        accessToken: process.env.CONTENTFUL_ACCESSTOKEN,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
     
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import { Social } from '../components'
+import { Social } from '.'
 
 const Footer = () => {
   return (
