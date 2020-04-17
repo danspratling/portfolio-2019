63373aeed68dbcbf935d3718098be7e74552f412
Dan Spratling <daniel.spratling@toumetis.com>
2020-04-17 22:07:04 +0100

    Uses contentful for projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react'
 import MagicGrid from 'react-magic-grid'
-import { Link } from 'gatsby'
-
-import dengro from '../images/dengro-preview-slice.png'
+import { graphql } from 'gatsby'
 
 import {
   Layout,
@@ -11,16 +9,22 @@ import {
   SectionIntro,
 } from '../components'
 
-const ProjectPage = () => {
-  const initialProjectList = [...Array(20).keys()]
+const ProjectPage = ({ data }) => {
+  //Get the page sections from the graphql data
+  const pageIntro = data.contentfulPage.intro
+  const projectList = data.allContentfulProject.nodes
+
+  //The page has an infinite list so create the length state
   const initialListLength = 7
   const [listLength, setListLength] = useState(initialListLength)
 
+  //updating the length triggers a rerender, so get the correct number of items to display
+  const projects = projectList.slice(0, listLength)
+
+  // update the state when the button is clicked
   const handleClick = () => {
     setListLength(listLength + initialListLength)
   }
-
-  const projects = initialProjectList.slice(0, listLength)
 
   return (
     <Layout>
@@ -29,21 +33,12 @@ const ProjectPage = () => {
       <section id="projects" className="min-h-screen bg-black py-32">
         <div className="container mx-auto">
           <div className="-mx-24">
+            {/* This is the magic which lets the masonry grid work */}
             <MagicGrid items={projects.length + 2} gutter={0}>
+              {/* The first item is always the intro component */}
               <div className="w-1/2 px-24 py-12">
                 <SectionIntro
-                  data={{
-                    heading: `Projects`,
-                    title: `Explore my work. Find something you love.`,
-                    body: `<p>
-                            With a portfolio of work ranging from brochure sites to 
-                            online stores, I'm a flexible developer able to help with
-                            any project you might need.`,
-                    link: {
-                      url: '/',
-                      title: 'Get in touch',
-                    },
-                  }}
+                  data={pageIntro}
                   animation={{
                     visibility: true,
                     direction: 'from left',
@@ -51,20 +46,16 @@ const ProjectPage = () => {
                 />
               </div>
 
+              {/* show cards for all the projects */}
               {projects.map(project => (
-                <div className="w-1/2 px-24 py-12">
-                  <ProjectCard
-                    data={{
-                      title: `Project ${project}.`,
-                      categories: ['Development', 'UX'],
-                      image: dengro,
-                    }}
-                  />
+                <div key={project.slug} className="w-1/2 px-24 py-12">
+                  <ProjectCard {...project} image={project.previewImage} />
                 </div>
               ))}
 
+              {/* The last element is a 'load more' button unless there are no more elements */}
               <div className="w-1/2 px-24 py-12 flex justify-center items-center">
-                {projects.length < initialProjectList.length && (
+                {projects.length < projectList.length && (
                   <button
                     onClick={handleClick}
                     className="w-auto h-auto bg-green-400 px-6 py-3 text-lg"
@@ -80,5 +71,41 @@ const ProjectPage = () => {
     </Layout>
   )
 }
+
+//Graphql query getting all the data we need from contentful (gatsby-config.js)
+export const query = graphql`
+  query {
+    contentfulPage(slug: { eq: "projects" }) {
+      intro {
+        heading
+        title
+        body {
+          json
+        }
+        link {
+          link
+          title
+        }
+      }
+    }
+    allContentfulProject {
+      nodes {
+        title
+        slug
+        categories
+        previewImage {
+          fluid {
+            base64
+            tracedSVG
+            srcWebp
+            srcSetWebp
+            srcSet
+            src
+          }
+        }
+      }
+    }
+  }
+`
 
 export default ProjectPage
