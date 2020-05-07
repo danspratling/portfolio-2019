62d243a7e9b10af7f63ca2117c5beb30e79b60a1
Dan Spratling <daniel.spratling@toumetis.com>
2020-05-07 10:02:35 +0100

    Improves OG images

--- functions/socialImage.js
+++ functions/socialImage.js
@@ -19,8 +19,13 @@ function generateImage({ title, slug }) {
   height = height - 50
 
   //fill an inner container to simulate a border
+  context.shadowOffsetX = 0
+  context.shadowOffsetY = 0
+  context.shadowBlur = 25
+  context.shadowColor = 'rgba(0,0,0,0.9)'
   context.fillStyle = '#000'
-  context.fillRect(25, 25, width, height)
+  roundRect(context, 25, 25, width, height, 15, true, false)
+  // context.fillRect(25, 25, width, height)
 
   //set the copy style
   context.font = 'bold 70pt Fira Code'
@@ -67,6 +72,40 @@ function generateImage({ title, slug }) {
   fs.writeFileSync(`static/${imageLocation}`, buffer)
 
   return imageLocation
+}
+
+function roundRect(ctx, x, y, width, height, radius, fill, stroke) {
+  if (typeof stroke === 'undefined') {
+    stroke = true
+  }
+  if (typeof radius === 'undefined') {
+    radius = 5
+  }
+  if (typeof radius === 'number') {
+    radius = { tl: radius, tr: radius, br: radius, bl: radius }
+  } else {
+    var defaultRadius = { tl: 0, tr: 0, br: 0, bl: 0 }
+    for (var side in defaultRadius) {
+      radius[side] = radius[side] || defaultRadius[side]
+    }
+  }
+  ctx.beginPath()
+  ctx.moveTo(x + radius.tl, y)
+  ctx.lineTo(x + width - radius.tr, y)
+  ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr)
+  ctx.lineTo(x + width, y + height - radius.br)
+  ctx.quadraticCurveTo(x + width, y + height, x + width - radius.br, y + height)
+  ctx.lineTo(x + radius.bl, y + height)
+  ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl)
+  ctx.lineTo(x, y + radius.tl)
+  ctx.quadraticCurveTo(x, y, x + radius.tl, y)
+  ctx.closePath()
+  if (fill) {
+    ctx.fill()
+  }
+  if (stroke) {
+    ctx.stroke()
+  }
 }
 
 module.exports = generateImage
