da8814e409eb28e09ec2ce5037038dc06e7cd0f5
Dan Spratling <daniel.spratling@toumetis.com>
2020-06-23 10:58:24 +0100

    Removes test link text

--- src/components/Link.js
+++ src/components/Link.js
@@ -18,7 +18,6 @@ const Link = ({ to, children }) => {
 //External links ar the default
 const ExternalLink = ({ link, children }) => (
   <a href={link} className={classes.join(' ')}>
-    {link}
     {children}
   </a>
 )
