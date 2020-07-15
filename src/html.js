0250901ac79eb458148f5356fb682b235b5c71af
Dan Spratling <daniel.spratling@toumetis.com>
2020-07-15 19:52:29 +0100

    Adds Fathom Analytics

--- src/html.js
+++ src/html.js
@@ -12,6 +12,11 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
+        <script
+          src="https://cdn.usefathom.com/script.js"
+          site="KNXTCNOS"
+          defer
+        ></script>
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
