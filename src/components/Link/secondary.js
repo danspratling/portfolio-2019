11f6d451f7f3468f4ab684213d04998341a393fc
Dan Spratling <daniel.spratling@toumetis.com>
2020-07-21 16:05:24 +0100

    New secondary button design

--- src/components/Link/secondary.js
+++ src/components/Link/secondary.js
@@ -9,13 +9,20 @@ const Secondary = ({ children, isHashLink }) => {
   }
 
   return (
-    <span className="text-lg text-white text-center md:text-left leading-loose mt-8 mb-6 mx-4 hover:text-green-400 transition duration-200 hover:underline">
-      {children}
+    <div className="relative inline-block pr-20 bg-white bg-opacity-0 hover:bg-opacity-10 mb-1 overflow-hidden transition duration-200 group">
+      <span className="inline-block w-auto h-auto px-6 md:px-8 py-3 md:py-4 text-lg text-white">
+        {children}
+      </span>
+      <span
+        className="absolute top-0 right-0 w-12 group-hover:w-20 bg-gray-800 bg-opacity-0 group-hover:bg-opacity-100 transition-all duration-300 transform origin-right"
+        style={{ height: '200%', '--transform-rotate': '20deg' }}
+      />
       <FontAwesomeIcon
         icon={faArrowRight}
-        className={`ml-6 ${isHashLink && 'transform rotate-90'}`}
+        className={`absolute top-1/2 right-0 text-xl text-white opacity-90 group-hover:opacity-100 z-10 mr-10 transition duration-300 transform translate-x-1/2 -translate-y-1/2 ${isHashLink &&
+          ' rotate-90'}`}
       />
-    </span>
+    </div>
   )
 }
 
