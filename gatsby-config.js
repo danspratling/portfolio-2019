18c44fa5a2d3afa30fe0db1c469e53fb51d81f4c
Dan Spratling <daniel.spratling@toumetis.com>
2020-07-31 10:34:22 +0100

    Adds redirects from .com to .dev

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-sitemap`,
+    `gatsby-plugin-netlify`,
     {
       resolve: `gatsby-plugin-prefetch-google-fonts`,
       options: {
